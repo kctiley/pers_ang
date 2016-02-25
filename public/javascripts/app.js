1a9f8cd3e11f260f2abadb4cb9e064a695767a7a
Klint Tiley <kctiley@gmail.com>
2016-02-25 13:12:21 -0700

    Initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('klinttiley', ['ngRoute'])
+  .config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider){
+    // '$httpProvider',$httpProvider not needed unless using services
+   $routeProvider
+    .when('/home', {
+      templateUrl: 'partials/home.html',
+      controller: 'homeController'
+    })
+    .when('/portfolio', {
+      templateUrl: 'partials/portfolio.html',
+      controller: 'portfolioController'
+    })
+    .when('/about', {
+        templateUrl: 'partials/about.html',
+        controller: 'aboutController'
+      })
+    .when('/contact', {
+      templateUrl: 'partials/contact.html',
+        controller: 'contactController'
+    })
+
+  $locationProvider.html5Mode(true);
+}])
---
+++ public/javascripts/controllers.js
@@ -0,0 +1,16 @@
+app.controller('homeController', ['$scope', function($scope) {
+
+}]);
+
+app.controller('portfolioController', ['$scope', function($scope) {
+
+}]);
+
+app.controller('aboutController', ['$scope', function($scope) {
+
+}]);
+
+app.controller('contactController', ['$scope', function($scope) {
+
+}]);
+
