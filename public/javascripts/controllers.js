0076697af8784e8c89ff6a9ae60a513d094beef6
Klint Tiley <kctiley@gmail.com>
2016-02-27 07:56:00 -0700

    Added view slides buttons and controller toggles between slides and previews

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -14,7 +14,16 @@ app.controller('homeController', ['$scope', function($scope) {
 }]);
 
 app.controller('portfolioController', ['$scope', function($scope) {
-
+  $scope.showPreviewsWrapper = true;
+  $scope.showSlidesWrapper = false;
+  $scope.slidesShow = function(){
+    $scope.showPreviewsWrapper = false;
+    $scope.showSlidesWrapper = true;
+  }
+  $scope.hideSlides = function(){
+    $scope.showSlidesWrapper = false;
+    $scope.showPreviewsWrapper = true;
+  }
 }]);
 
 app.controller('aboutController', ['$scope', function($scope) {
